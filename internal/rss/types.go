10f695f9cc62c6aecd17ce93447abf7a2decc554
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-15 15:13:45 -0400

    feat: Add RSS feed types

---
+++ internal/rss/types.go
@@ -0,0 +1,19 @@
+package rss
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+type RSSChannel struct {
+	Title       string    `xml:"title"`
+	Link        string    `xml:"link"`
+	Description string    `xml:"description"`
+	Item        []RSSItem `xml:"item"`
+}
+
+type RSSFeed struct {
+	Channel RSSChannel `xml:"channel"`
+}
