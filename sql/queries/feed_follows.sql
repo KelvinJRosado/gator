48accdf66837633d1c5c7cd2641ba94850f98347
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-15 19:33:07 -0400

    feat: Add feed follows table and operations

---
+++ sql/queries/feed_follows.sql
@@ -0,0 +1,20 @@
+-- name: CreateFeedFollow :one
+WITH
+  new_row as (
+    INSERT INTO
+      feed_follows (id, created_at, updated_at, user_id, feed_id)
+    VALUES
+      ($1, $2, $3, $4, $5)
+    RETURNING
+      *
+  )
+SELECT
+  new_row.id,
+  new_row.created_at,
+  new_row.updated_at,
+  users.name as user_name,
+  feeds.name as feed_name
+FROM
+  new_row
+  INNER JOIN users ON feed_follows.user_id = users.id
+  INNER JOIN feeds on feed_follows.feed_id = feeds.id;
