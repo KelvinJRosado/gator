bb13b9d7aada77f80676307e296c4f9ca9b9ed28
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-15 15:42:10 -0400

    feat: Implement RSS feed fetching with HTTP client

--- internal/rss/rss.go
+++ internal/rss/rss.go
@@ -2,8 +2,35 @@ package rss
 
 import (
 	"context"
+	"encoding/xml"
+	"io"
+	"net/http"
 )
 
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, err
+	}
 
+	req.Header.Set("User-Agent", "gator")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var feed *RSSFeed
+	err = xml.Unmarshal(data, feed)
+	if err != nil {
+		return nil, err
+	}
+
+	return feed, nil
 }
