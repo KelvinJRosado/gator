088db033d88084a8dcddb16ff8b52902907fb919
KelvinJRosado <kelvinrosado1@yahoo.com>
2026-05-16 18:27:29 -0400

    feat: Add upsert behavior to posts insert

--- internal/database/posts.sql.go
+++ internal/database/posts.sql.go
@@ -27,6 +27,7 @@ INSERT INTO
   )
 VALUES
   ($1, $2, $3, $4, $5, $6, $7, $8)
+ON CONFLICT (url) DO NOTHING
 RETURNING
   id, created_at, updated_at, title, url, description, published_at, feed_id
 `
--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -12,6 +12,7 @@ INSERT INTO
   )
 VALUES
   ($1, $2, $3, $4, $5, $6, $7, $8)
+ON CONFLICT (url) DO NOTHING
 RETURNING
   *;
 
